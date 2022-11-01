81297ab10e8dca91027b029e7cf5d6589efef1b6
rifatbiswas <rifatbiswas2@gmail.com>
2022-11-01 23:15:36 +0600

    v-26 & 27

---
+++ v-26.js
@@ -0,0 +1,15 @@
+//====== Object ===========
+
+
+var sakib={
+    name:'sakil al hassan',
+    age:30,
+    country:'Bangladesh ',
+    AllRounder:'true',
+    caotain:'true',
+
+}
+//console.log(sakib.country);  // dot notation......
+//console.log(sakib['country']); // bracket notation...
+//console.log(Object.keys(sakib));  // only for propertis....
+//console.log(Object.values(sakib)); // only for value....
