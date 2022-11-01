97eda8c4280219524431a666c1224c1b761cc7c2
rifatbiswas <rifatbiswas2@gmail.com>
2022-11-01 22:10:03 +0600

    v-22 & v-24

---
+++ v-22.js
@@ -0,0 +1,18 @@
+//========== Array value Remove & Add =============
+
+var countryName=['Bangladesh','India','Srilanka','Napal','Chaina','USA','Canada','Africa'];
+
+// countryName.push('Pakisthan'); // Add value last
+
+//countryName.unshift('Koria') //  Add value first
+
+//countryName.pop(); // Remove value last
+
+//countryName.shift(); //  Remove value first
+
+
+
+ console.log(countryName); 
+
+
+
---
+++ v-24.js
@@ -0,0 +1,8 @@
+//=========== Array element find with loop========
+
+var number =[14,52,32,14,47,78,95,72,15,43,20,]
+
+for(i=0;i<number.length;i++){
+
+    console.log(number[i]);
+}
